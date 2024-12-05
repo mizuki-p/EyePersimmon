a8af20c9174474af522c8b15efe90b411e4b1e1a
muxiangwen <1750256410@qq.com>
2024-12-05 16:46:39 +0800

    Flask->WebSocket,完善model.py和测试代码

--- model_server/flask_app/app/model.py
+++ model_server/flask_app/app/model.py
@@ -1,12 +1,34 @@
-import numpy as np
+from transformers import BlipProcessor, BlipForImageTextRetrieval
+from PIL import Image
 
-def load_model():
-    """加载机器学习模型"""
-    print("Model loaded successfully!")
-    return "mock_model"  
+# import asyncio
 
-def model_inference(observation, instruction):
-    """模拟模型推理"""
-    
-    print(f"Performing inference ......")
-    return list(np.random.rand(3))# action_vector
+
+class BlipInference:
+    def __init__(self, model_path, processor_path):
+        """加载模型和处理器"""
+        self.processor = BlipProcessor.from_pretrained(processor_path)
+        self.model = BlipForImageTextRetrieval.from_pretrained(model_path).to("cuda")
+        print("Model and processor loaded successfully!")
+
+    def infer(self, observation, instruction):
+        """推理逻辑"""
+        raw_image = Image.open(observation)
+        inputs = self.processor(raw_image, instruction, return_tensors="pt").to("cuda")
+        print(f"Performing inference ......")
+        itm_scores = self.model(**inputs)[0]
+        res = itm_scores.tolist()[0]  # 转为 Python 列表
+        return res
+
+
+# 初始化模型
+inference_engine = BlipInference(
+    model_path="/data/nvme2/dky/lc/EyePersimmon/blip-itm-base-coco",
+    processor_path="/data/nvme2/dky/lc/EyePersimmon/blip-itm-base-coco",
+)
+
+# # 测试推理
+# observation = "/data/nvme2/dky/lc/image_1.png"
+# instruction = "a man is talking to someone."
+# res = inference_engine.infer(observation, instruction)
+# print(res)
